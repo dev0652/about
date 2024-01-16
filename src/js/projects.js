6d67f153eb054da18e54b59acaa71683fd2298f3
dev0652 <113797010+dev0652@users.noreply.github.com>
2024-01-16 22:06:35 +0200

    fix image paths

--- src/js/projects.js
+++ src/js/projects.js
@@ -7,9 +7,12 @@ const handleMissingImage = ({ currentTarget }) => {
   currentTarget.src = 'https://placehold.co/400';
 };
 
+const makeImagePath = (path) => new URL(path, import.meta.url).href;
+
 const makeItemMarkup = (project) => {
   const { name, type, link, thumb, description } = project;
   // const imagePath = thumb !== '' ? thumb : placeholderImage;
+  const imagePath = makeImagePath(thumb);
 
   return `
    <li class="project-card ">
@@ -19,7 +22,7 @@ const makeItemMarkup = (project) => {
         <div class="flip-card-front">
           <img
             class="project-card-image"
-            src="${thumb}"
+            src="${imagePath}"
             alt="${name} live page screenshot"
             loading="lazy"
             onError="${handleMissingImage}"
