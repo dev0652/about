f8b9eecf9d59f855cda451c31c6c0d18b28bb904
dev0652 <113797010+dev0652@users.noreply.github.com>
2024-03-19 20:30:31 +0200

    adds scrolling capability for project card modal at low viewport heights

--- src/js/createCardModal.js
+++ src/js/createCardModal.js
@@ -22,60 +22,59 @@ export function createCardModal(project) {
   const technologiesList = technologies.join(', ');
 
   return /* html */ `
-      <article>
-        <h2 class="modal-title">${name}</h2>
+    <article class="modal-card">
+      <h2 class="modal-title">${name}</h2>
 
-        <div class="modal-content-wrapper">
+      <div class="modal-content-wrapper">
 
-          <div class="modal-summary">
-            <div class="summary-columns-wrapper">
-              <div class="summary-column">
-                <p class="type"><span class="field-type">Project type:</span> ${type}</p>
+        <div class="modal-summary">
+          <div class="summary-columns-wrapper">
+            <div class="summary-column">
+              <p class="type"><span class="field-type">Project type:</span> ${type}</p>
 
-                <p class="stack"><span class="field-type">Stack:</span> ${stack}</p>
-              </div>
-
-              <div class="summary-column">
-                <p class="role"
-                style="${!role && 'display: none'}">
-                  <span class="field-type">Role:</span> ${role}
-                </p>
-
-                <p class="customer"
-                style="${!customer && 'display: none'}">
-                  <span class="field-type">Customer:</span> ${customer}
-                </p>
-              </div>
+              <p class="stack"><span class="field-type">Stack:</span> ${stack}</p>
             </div>
 
-            <p class="technologies">${technologiesList}</p>
+            <div class="summary-column">
+              <p class="role"
+              style="${!role && 'display: none'}">
+                <span class="field-type">Role:</span> ${role}
+              </p>
+
+              <p class="customer"
+              style="${!customer && 'display: none'}">
+                <span class="field-type">Customer:</span> ${customer}
+              </p>
+            </div>
           </div>
 
-          <div class="modal-image-block">${modalPictureTag}</div>
-
-          <div class="modal-description">
-            <p class="project-description">${description}</p>
-          </div>
-          
-          <div class="modal-project-links-wrapper">
-            <a
-              class="modal-project-link"
-              href="${link}"
-              target="_blank"
-              rel="noopener noreferrer"
-              >GitHub</a
-            >
-
-            <a
-              class="modal-project-link"
-              href="${livePage}"
-              target="_blank"
-              rel="noopener noreferrer"
-              >Live page</a
-            >
-          </div>
-
+          <p class="technologies">${technologiesList}</p>
         </div>
-      </article>
+
+        <div class="modal-image-block">${modalPictureTag}</div>
+
+        <div class="modal-description">
+          <p class="project-description">${description}</p>
+        </div>
+      </div>
+
+      <div class="modal-project-links-wrapper">
+        <a
+          class="modal-project-link"
+          href="${link}"
+          target="_blank"
+          rel="noopener noreferrer"
+          >GitHub</a
+        >
+
+        <a
+          class="modal-project-link"
+          href="${livePage}"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Live page</a
+        >
+      </div>
+    </article>
   `;
 }
